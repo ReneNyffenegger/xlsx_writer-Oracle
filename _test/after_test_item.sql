789db40a2594180196e289f06e0d2a2b2b6a484f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-11-04 15:14:53 +0100

    Compare gotten files with expected ones

--- _test/after_test_item.sql
+++ _test/after_test_item.sql
@@ -1,6 +1,11 @@
 define step=&1
 prompt &step
 
-$&test_out_dir\&step..xlsx
+-- Open created xls
+-- $&test_out_dir\&step..xlsx
+
 $cmd /c rmdir /s /q gotten\&step
 $&unzip_cmd &test_out_dir\&step..xlsx gotten\&step
+
+-- Diff expected with what was written
+$diff -r expected\&step gotten\&step
